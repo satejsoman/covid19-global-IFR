ab878bad9647284fe715449750f7b06da1aa043d
satej soman <satej.soman@gmail.com>
2021-07-14 09:38:51 -0500

    Update choropleth.py

--- choropleths/choropleth.py
+++ choropleths/choropleth.py
@@ -9,6 +9,8 @@ import seaborn as sns
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from shapely.wkt import loads
 
+sns.set(font = "Helvetica Neue", style = "white")
+
 # file locations
 results_path = "~/Downloads/avgIFR.csv"
 try: 
@@ -19,33 +21,42 @@ except NameError:
 # titles, labels, etc.
 title = "mean IFR by study location"
 
-sns.set(font = "Helvetica Neue", style = "white")
-
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-world = world[world.continent != "Antarctica"]
-
+# load IFR data and join against GADM geometries
+ifr = pd.read_csv(results_path)\
+    .set_index("location_id")\
+    .drop(columns = ["country", "location", 'location_id.1', 'country.1', 'location.1'])
 gdf = pd.read_csv(pwd / "IFR_geometries.csv")\
     .set_index("location_id")\
     .assign(geometry = lambda _:_["geometry"].apply(loads))\
     .drop(columns = ["Unnamed: 0"])\
-    .pipe(gpd.GeoDataFrame)
-IFR = pd.read_csv(results_path)\
-    .set_index("location_id")\
-    .drop(columns = ["country", "location", 'location_id.1', 'country.1', 'location.1'])
-gdf = gdf.join(IFR).assign(log_ifr = lambda _: np.log(_["Mean"]))
+    .pipe(gpd.GeoDataFrame)\
+    .join(ifr)\
+    .assign(log_ifr = lambda _: np.log(_["Mean"]))
 
+# set colorscheme for choropleth
 sm = mpl.cm.ScalarMappable(
     norm = mpl.colors.Normalize(gdf.log_ifr.min(), gdf.log_ifr.max()), 
-    cmap = "YlOrRd"
+    cmap = "YlOrRd" # see https://matplotlib.org/stable/tutorials/colors/colormaps.html for other colorschemes
 )
 
+# set up plotting
 fig, ax = plt.subplots()
 plt.gca().axis("off")
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size = "2.5%", pad = 0.5)
 
-world.plot(ax = ax, facecolor = "dimgrey", edgecolor = "white", linewidth = 0.70, zorder = -100, alpha = 0.5)
+# plot background map (sans Antarctica)
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+world[world.continent != "Antarctica"].plot(
+    ax = ax, 
+    facecolor = "dimgrey", 
+    edgecolor = "white", 
+    linewidth = 0.70, 
+    zorder = -100, 
+    alpha = 0.5
+)
 
+# plot log(IFR) for all locations to ensure the colorbar captures the full range of values
 gdf.plot(
     ax = ax, 
     column = "log_ifr", 
@@ -57,16 +68,25 @@ gdf.plot(
     linewidth = 0
 )
 
-gdf[~gdf.display_as_pt].plot(ax = ax, facecolor = "none", edgecolor = "white", linewidth = 0.5)
+# plot white border for non-point locations
+gdf[~gdf.display_as_pt].plot(
+    ax = ax, 
+    facecolor = "none", 
+    edgecolor = "white", 
+    linewidth = 0.5
+)
 
+# plot point locations with exaggerated markers
 gdf[gdf.display_as_pt].representative_point().plot(
     ax = ax, 
     color = [sm.to_rgba(_) for _  in gdf[gdf.display_as_pt].log_ifr], 
     marker = 'o', 
     markersize = 50, 
     linewidth = 0.5, 
-    edgecolor = "white")
+    edgecolor = "white"
+)
 
+# show map
 plt.suptitle(f"\n{title}")
 plt.subplots_adjust(left = 0)
 plt.show()
